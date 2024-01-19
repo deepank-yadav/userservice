58cca6388618001dded75a67aa05020419c3bd3d
deepank <deepankkyadav@gmail.com>
2024-01-19 22:59:11 +0530

    Added user auuthentication template

--- src/main/java/com/scaler/userservice/controllers/UserAuthentication.java
+++ src/main/java/com/scaler/userservice/controllers/UserAuthentication.java
@@ -1,9 +1,17 @@
 package com.scaler.userservice.controllers;
 
+import com.scaler.userservice.models.User;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
 public class UserAuthentication {
+
+    @PostMapping("/login")
+    public String loginUser(@RequestBody User user){
+        return "token";
+    }
 }
