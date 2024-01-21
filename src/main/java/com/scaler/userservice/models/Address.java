dd2adde3ed551f7fe7c94aaa1dad40474be4fb84
deepank <deepankkyadav@gmail.com>
2024-01-21 23:21:32 +0530

    Added Geolocation

--- src/main/java/com/scaler/userservice/models/Address.java
+++ src/main/java/com/scaler/userservice/models/Address.java
@@ -1,8 +1,13 @@
 package com.scaler.userservice.models;
 
+import jakarta.persistence.OneToOne;
+
 public class Address {
 
     private String street;
     private String city;
     private String pincode;
+
+    @OneToOne
+    private Geolocation geolocation;
 }
