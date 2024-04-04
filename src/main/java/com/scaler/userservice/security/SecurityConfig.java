3d4b5d6912c3f724821442603ac8cbaff4b37f37
deepank <deepankkyadav@gmail.com>
2024-04-04 07:29:18 +0530

    Implemented OAuth2

---
+++ src/main/java/com/scaler/userservice/security/SecurityConfig.java
@@ -0,0 +1,2 @@
+package com.scaler.userservice.security;public class SecurityConfig {
+}
---
+++ src/main/java/com/scaler/userservice/security/repository/AuthorizationConsentRepository.java
@@ -0,0 +1,2 @@
+package com.scaler.userservice.security.repository;public interface AuthorizationConsentRepository {
+}
