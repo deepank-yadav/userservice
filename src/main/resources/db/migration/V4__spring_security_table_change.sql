cafe5cc57a33be0ef669b0b1987a209be09bdbb9
deepank <deepankkyadav@gmail.com>
2024-04-06 02:06:54 +0530

    User Authentication Completed

---
+++ src/main/resources/db/migration/V4__spring_security_table_change.sql
@@ -0,0 +1,47 @@
+ALTER TABLE authorization
+    MODIFY access_token_metadata LONGTEXT;
+
+ALTER TABLE authorization
+    MODIFY access_token_scopes LONGTEXT;
+
+ALTER TABLE authorization
+    MODIFY access_token_value LONGTEXT;
+
+ALTER TABLE authorization
+    MODIFY attributes LONGTEXT;
+
+ALTER TABLE authorization
+    MODIFY authorization_code_value LONGTEXT;
+
+ALTER TABLE authorization
+    MODIFY authorized_scopes LONGTEXT;
+
+ALTER TABLE authorization
+    MODIFY device_code_metadata LONGTEXT;
+
+ALTER TABLE authorization
+    MODIFY device_code_value LONGTEXT;
+
+ALTER TABLE authorization
+    MODIFY oidc_id_token_claims LONGTEXT;
+
+ALTER TABLE authorization
+    MODIFY oidc_id_token_metadata LONGTEXT;
+
+ALTER TABLE authorization
+    MODIFY oidc_id_token_value LONGTEXT;
+
+ALTER TABLE authorization
+    MODIFY refresh_token_metadata LONGTEXT;
+
+ALTER TABLE authorization
+    MODIFY refresh_token_value LONGTEXT;
+
+ALTER TABLE authorization
+    MODIFY state LONGTEXT;
+
+ALTER TABLE authorization
+    MODIFY user_code_metadata LONGTEXT;
+
+ALTER TABLE authorization
+    MODIFY user_code_value LONGTEXT;
